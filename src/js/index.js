9d3f4230c0925bbc7ff4ca2f8430635f7390d1ae
petriwna <petriwna43@gmail.com>
2025-02-17 16:12:48 +0200

    feat: added firebase hosting and fix style

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 import '../scss/style.scss';
 
+import { initializeApp } from 'firebase/app';
+
 import { Main } from './Main';
 
 export function init() {
   new Main();
+
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
 }
 
 document.addEventListener('DOMContentLoaded', init);
